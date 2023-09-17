1c4c5a69d2488a6ddc5e2605e739a29091708885
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-09-16 21:59:52 -0300

    convert to pug template

--- routes/shop.js
+++ routes/shop.js
@@ -11,8 +11,13 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     // console.log("Another middleware");
     /* res.sendFile(path.join(__dirname, '../' ,'views', 'shop.html')); */
-    console.log(adminData.products)
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+
+/*     console.log(adminData.products)
+    res.sendFile(path.join(rootDir, 'views', 'shop.html')); */
+
+    const products = adminData.products
+
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 })
 
 // get looks for exact matching of path
